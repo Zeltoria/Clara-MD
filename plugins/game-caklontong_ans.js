84307523b40c6ffd64e87ce2ca981ca9407f3b9c
Zeltoria <123985221+Zeltoria@users.noreply.github.com>
2023-06-09 18:35:45 +0700

    Update game-caklontong_ans.js

--- plugins/game-caklontong_ans.js
+++ plugins/game-caklontong_ans.js
@@ -2,7 +2,6 @@ import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    let benarnya = 'https://telegra.ph/file/ab725c3de31e39bf3a08a.jpg'
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/Ketik.*calo/i.test(m.quoted.text) || /.*(calo|bantuan)/i.test(m.text))
         return !0
     this.caklontong = this.caklontong ? this.caklontong : {}
@@ -12,7 +11,7 @@ export async function before(m) {
         let json = JSON.parse(JSON.stringify(this.caklontong[id][1]))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.caklontong[id][2]
-            await this.sendFile(m.chat, benarnya, 'benar.jpg', `*Benar!* +${this.caklontong[id][2]} XP\n${json.deskripsi}`, m)
+            await this.reply(m.chat, `*Benar!* +${this.caklontong[id][2]} XP\n${json.deskripsi}`, m)
             clearTimeout(this.caklontong[id][3])
             delete this.caklontong[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
