9add05c498b2710d1404242168fd27878556273c
JEFRI ANUGRAH <128717477+jefria@users.noreply.github.com>
2023-06-19 23:41:57 +0700

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,12 +1,22 @@
-import fetch from 'node-fetch'
-let handler = async (m, {text, args}) => {
-  if (!args[0]) throw `Use example .simi halo`
-  let api = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=id`)
-  let res = await api.json()
-  m.reply(res.success)
-}
-handler.command = ['simi']
-handler.tags = ['fun']
-handler.help = ['simi']
+import fetch from 'node-fetch';
 
-export default handler
+let handler = m => m;
+
+handler.before = async (m) => {
+  let chat = global.db.data.chats[m.chat];
+  if (chat.simi && !chat.isBanned) {
+    if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
+    if (!m.text) return;
+    let api = await fetch(`https://api.itsrose.life/others/simi?message=${encodeURIComponent(m.text)}&level=12&lc=id&apikey=${global.rose}`);
+    if (!api.ok) throw new Error('Error fetching data');
+    let res = await api.json();
+    if (res.result && res.result.simi && res.result.simi.sentence) {
+      return m.reply(res.result.simi.sentence);
+    } else {
+      return m.reply('Tidak ada respons yang valid.');
+    }
+  }
+  return true;
+};
+
+export default handler;
