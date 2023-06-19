c6a541307f2d32cb7f58a54d7162631f45221e88
JEFRI ANUGRAH <128717477+jefria@users.noreply.github.com>
2023-06-19 23:37:46 +0700

    FIx Simi
    Ubah di line 4
    level= [ 1-12 ]
    lc= [ id, en, dll ]

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch'
 let handler = async (m, {text, args}) => {
-  if (!args[0]) throw `Use example .simi halo`
-  let api = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=id`)
+  if (!args[0]) throw `Use example .simi halo kak`
+  let api = await fetch(`https://api.itsrose.life/others/simi?message=${text}&level=12&lc=id&apikey=${global.rose}`)
   let res = await api.json()
-  m.reply(res.success)
-}
+  m.reply(res.result.simi.sentence)
+  }
 handler.command = ['simi']
 handler.tags = ['fun']
 handler.help = ['simi']
