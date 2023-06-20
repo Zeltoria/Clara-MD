f5026f9f03f278d2d3aa1ca0850ee5f8e60a6d37
Xyroinee-ID <123985221+Xyroinee@users.noreply.github.com>
2023-06-20 17:47:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,29 +6,34 @@ const readMore = more.repeat(4001)
 
 // Owner
 global.owner = [
-['6285760451683', 'Xyroinee', true]
+['6285760451683', 'Zeltoria', true]
 ]
 global.mods = []
 global.prems = []
-// Info 
+// Info
 global.nomorwa = '6285760451683'
-global.author = '© Xyroine'
+global.author = '© Xyroinee'
+global.namebot = '​Clara - MD'
+global.wm = '© Clara By Xyroine'
 global.stickpack = 'Clara - MD'
 global.stickauth = '© Xyroine'
-global.packname = 'Clara - MD'
-global.namebot = '​Clara - MD'
-global.wm = 'Clara by Xyroine'
-// Message
+// Link Sosmed
+global.sig = 'https://www.instagram.com/danilelistz02'
+global.sgh = 'https://github.com/Xyroinee'
+global.sgc = 'https://chat.whatsapp.com/LAWdMIQri9SKee5kqmISH0'
+// Donasi
+global.psaweria = 'https://saweria.co/Zeltoria'
+global.ptrakterr = 'https://trakteer.id/zeltoria'
+global.povo = '0896-1075-0607'
+// Info Wait
 global.wait = '_Sedang Di Proses, Mohon Tunggu_....'
 global.eror = 'Terjadi Kesalahan Coba Lagi Nanti!'
-// Level Rpg
 global.multiplier = 69 
 // Apikey
-global.xcode = 'Apikeylu'
-global.xyro = 'Apikeylu'
-// Note: Jika Tidak Punya Apikey Silahkan Daftar Ke Website Ini
-// Xyro: https://api.xyroinee.xyz
-// Xcode: https://api-xcoders.site
+global.xyro = 'apikeylu'
+// Website: https://api.xyroinee.xyz
+// Daftar Ke Website Tersebut Untuk..
+// Mendapatkan Apikey Kamu
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
@@ -36,3 +41,4 @@ watchFile(file, () => {
   console.log(chalk.redBright("Update 'config.js'"))
   import(`${file}?update=${Date.now()}`)
 })
+
