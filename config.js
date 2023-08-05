b2911e6ce855cd137f98c97549a30ad14ec80d9e
Xyroinee-ID <123985221+Xyroinee@users.noreply.github.com>
2023-08-06 05:00:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,39 +6,56 @@ const readMore = more.repeat(4001)
 
 // Owner
 global.owner = [
-['6285760451683', 'Zeltoria', true]
+['628xxx', 'Xyroinee', true]
 ]
 global.mods = []
 global.prems = []
 // Info
-global.nomorwa = '6285760451683'
+global.nomorwa = '628xxx'
+global.packname = 'anu - MD by'
 global.author = '© Xyroinee'
-global.namebot = '​Clara - MD'
-global.wm = '© Clara By Xyroine'
-global.stickpack = 'Clara - MD'
+global.namebot = '​anu - MD'
+global.wm = '© anu By Xyroine'
+global.stickpack = 'anu - MD by'
 global.stickauth = '© Xyroine'
+global.fotonya = 'https://telegra.ph/file/83b522fd80b660705d1cd.jpg'
 // Link Sosmed
-global.sig = 'https://www.instagram.com/danilelistz02'
+global.sig = 'https://www.instagram.com/xyroinee'
 global.sgh = 'https://github.com/Xyroinee'
-global.sgc = 'https://chat.whatsapp.com/LAWdMIQri9SKee5kqmISH0'
+global.sgc = 'https://chat.whatsapp.com/'
 // Donasi
 global.psaweria = 'https://saweria.co/Zeltoria'
 global.ptrakterr = 'https://trakteer.id/zeltoria'
 global.povo = '0896-1075-0607'
 // Info Wait
-global.wait = '_Sedang Di Proses, Mohon Tunggu_....'
+global.wait = 'Sedang Di Proses, Mohon Tunggu....'
 global.eror = 'Terjadi Kesalahan Coba Lagi Nanti!'
 global.multiplier = 69 
 // Apikey
-global.xyro = 'apikeylu'
+global.lol = 'apikeymu'
+global.rose = 'apikeymu'
+global.xyro = 'apikeymu'
+// Catatan : Jika Mau Work Fiturnya
+// Masukan Apikeymu
+// Gapunya Apikey? Ya Daftar
 // Website: https://api.xyroinee.xyz
-// Daftar Ke Website Tersebut Untuk..
+// Daftar Ke Website Tersebut Untuk
 // Mendapatkan Apikey Kamu
+global.APIs = {
+    xyro: "https://api.xyroinee.xyz",
+    popcat : 'https://api.popcat.xyz'
+}
+
+/Apikey/
+global.APIKeys = {
+    "https://api.xyroinee.xyz": "apikeymu",
+}
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
   console.log(chalk.redBright("Update 'config.js'"))
   import(`${file}?update=${Date.now()}`)
+})
 })
 
